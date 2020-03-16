9b62199a110a6b56540167b9f59702997f3e7abb
Junko Tahara <junkotahara911@gmail.com>
2020-03-16 17:20:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,8 +12,8 @@ def monopoly_with_second_tier
   # complexity.
   hash[:railroads] = {
     :pieces => 4,
-    :rent_in_dollars = { },
-    :names = { }
+    :rent_in_dollars => { },
+    :names => { }
   }
 end
 
