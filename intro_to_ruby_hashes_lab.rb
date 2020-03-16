48ceee50ca209d7146f22320149c3a30afb5c0ad
Junko Tahara <junkotahara911@gmail.com>
2020-03-16 17:26:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,6 +22,10 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  hash[:railroads][:rent_in_dollars] = {
+    :one_piece_owned => 25,
+    :
+  }
 
 end
 
